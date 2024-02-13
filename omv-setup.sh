296a62f448d5e4f8666d2aed0136b5c6db15ac71
Bogdan Arsene <bogdan.arsene@gmail.com>
2024-02-13 03:32:28 +0200

    omv-extras + flashplugin

--- omv-setup.sh
+++ omv-setup.sh
@@ -3,7 +3,13 @@
 # Enable printing of commands
 set -x
 
+# Disable Lid Switch
 bash set-config.sh /etc/systemd/logind.conf HandleLidSwitch ignore
+
+# Install OMV-Extras
+wget -O - https://github.com/OpenMediaVault-Plugin-Developers/packages/raw/master/install | bash
+# Install FlashMemory Plugin
+apt-get install openmediavault-flashmemory
 
 # Disable printing of commands
 #set +x
