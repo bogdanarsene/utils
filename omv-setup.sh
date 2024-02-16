949779542e6d519b16dba157511db34129216874
Bogdan Arsene <bogdan.arsene@gmail.com>
2024-02-16 21:23:57 +0200

    add sensors

--- omv-setup.sh
+++ omv-setup.sh
@@ -6,10 +6,15 @@ set -x
 # Disable Lid Switch
 bash set-config.sh /etc/systemd/logind.conf HandleLidSwitch ignore
 
+sudo apt-get update
+
 # Install OMV-Extras
 wget -O - https://github.com/OpenMediaVault-Plugin-Developers/packages/raw/master/install | bash
 # Install FlashMemory Plugin
 apt-get install openmediavault-flashmemory
+
+sudo apt-get install lm-sensors
+sudo sensors-detect
 
 # Disable printing of commands
 #set +x
